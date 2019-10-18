83a60b52f216043d4b83fbfdfe66f075f2e4993a
Harry Blakiston Houston <harry@phoneboxapp.co.uk>
2019-10-18 18:55:54 +0100

    Lab 3 Linear Regression

---
+++ Lab2/MLLab2.py
@@ -0,0 +1,54 @@
+import numpy as np
+from scipy.stats import beta
+import matplotlib.pyplot as plt
+
+def posterior(a,b,X):
+    a_n = a + X.sum()
+    b_n = b + (X.shape[0]-X.sum())
+    
+    return beta.pdf(mu_test, a_n,b_n)
+
+
+def posteriorMean(a, b):
+	print(a,b)
+	return
+
+
+#parameters to generate data
+mu = 0.2
+N = 100
+
+#generate some date
+X = np.random.binomial(1, mu, N)
+mu_test = np.linspace(0,1,100)
+
+# now lets define our prior
+a=25
+b=100
+
+#p (mu) = beta(alpha, beta)
+prior_mu = beta.pdf(mu_test,a,b)
+
+#create figure
+fig = plt.figure(figsize=(10,5))
+ax = fig.add_subplot(111)
+
+#plot prior
+ax.plot(mu_test, prior_mu, 'g')
+ax.fill_between(mu_test,prior_mu,color='green',alpha=0.3)
+ax.set_xlabel('$\mu$')
+ax.set_ylabel('$p(\mu|\mathbf{x})$')
+
+#lets pick a random (uniform) point from the data
+# and update our assumption with this
+
+index = np.random.permutation(X.shape[0])
+for i in range (0, X.shape[0]):
+	y=posterior(a,b,X[:index[i]])
+	posteriorMean=posteriorMean(a, b)
+	plt.plot(i, y, 'r', alpha=0.3)
+
+y=posterior(a,b,X)
+plt.plot(mu_test,y,'b',linewidth=4.0) 
+
+plt.show(block=True)
---
+++ Lab3/linear_regression.py
@@ -0,0 +1,54 @@
+"""Create a contour plot of a two-dimensional normal distribution
+
+Parameters
+__________
+ax : acis handle to plot
+mu : mean vector 2x1
+Sigma : covariance matric 2x2
+
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+
+def plot_distribution(ax,mu,Sigma):
+    x=np.linspace(-1.5, 1.5,100)
+    x1p, x2p = np.meshgrid(x,x)
+    pos = np.vstack((x1p.flatten(), x2p.flatten())).T
+    pdf = np.random.multivariate_normal(mu.flatten(), Sigma)
+    Z=pdf.pdf(pos)
+    Z=Z.reshape(100,100)
+
+    ax.contour(x1p,x2p,Z,5,colors='r',lw=5,alpha=0.7)
+    ax.set_xlabel('w_0')
+    ax.set_ylabel('x_1')
+
+    return
+
+#create prior distribution
+tau=1*np.eye(2)
+w_0=np.zeros((2,1))
+
+#Create prior weights
+w = np.array([-1.3,0.3]).T
+
+#Generate data X
+X_c=np.linspace(-1,1,10)
+X=[]
+for i in range(len(X_c)):
+    X.append([X_c[i], 1])
+
+#Generate data Y
+computeY = lambda x: w.T*x+np.random.normal(0,0.3)
+Y = np.array([computeY(xi) for xi in X])
+
+#Create fig
+fig=plt.figure()
+ax=fig.add_subplot(111)
+
+#visualise distribution over w
+plot_distribution(ax,w_0,tau)
+fig.show()
+
+
+
