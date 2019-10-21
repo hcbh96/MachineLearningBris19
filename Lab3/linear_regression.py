b34ad4e7c7a0d52c914e15bca575bb70ab04da73
Harry Blakiston Houston <harry@phoneboxapp.co.uk>
2019-10-21 10:08:42 +0100

    2d contour plot now working

--- Lab3/linear_regression.py
+++ Lab3/linear_regression.py
@@ -9,17 +9,17 @@ Sigma : covariance matric 2x2
 """
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import norm
+from scipy.stats import multivariate_normal
 
 def plot_distribution(ax,mu,Sigma):
     x=np.linspace(-1.5, 1.5,100)
     x1p, x2p = np.meshgrid(x,x)
     pos = np.vstack((x1p.flatten(), x2p.flatten())).T
-    pdf = np.random.multivariate_normal(mu.flatten(), Sigma)
+    pdf = multivariate_normal(mu.flatten(), Sigma)
     Z=pdf.pdf(pos)
     Z=Z.reshape(100,100)
 
-    ax.contour(x1p,x2p,Z,5,colors='r',lw=5,alpha=0.7)
+    ax.contour(x1p,x2p,Z,5,colors='r',alpha=0.7)
     ax.set_xlabel('w_0')
     ax.set_ylabel('x_1')
 
@@ -48,7 +48,7 @@ ax=fig.add_subplot(111)
 
 #visualise distribution over w
 plot_distribution(ax,w_0,tau)
-fig.show()
+plt.show()
 
 
 
